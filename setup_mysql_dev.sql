2f572b5e7cbe0ebfc392cec3f12f09974b8ed28e
Ebube-Ochemba <ebube17@gmail.com>
2024-04-17 16:00:05 +0100

    correction

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,8 @@
+-- Write a script that prepares a MySQL server for the project:
+
+
 -- Create the database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
--- Use the new database
-USE hbnb_dev_db;
 
 -- Create the user if it does not exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
@@ -12,3 +12,5 @@ GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grant SELECT privilege on the performance_schema database to the user hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+FLUSH PRIVILEGES;
