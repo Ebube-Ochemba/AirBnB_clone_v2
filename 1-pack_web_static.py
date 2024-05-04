0a46490fa19aa2e80297eba3a616f01fd334eeda
Ebube-Ochemba <ebube17@gmail.com>
2024-05-05 00:49:35 +0100

    checker

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A Facric Script that creates a .tgz archive from the
+contents of the 'web_static' folder.
+"""
+
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir
+
+
+def do_pack():
+    """Creates a .tgz archive"""
+    try:
+        local("mkdir -p versions")
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        filename = "versions/web_static_{}.tgz".format(timestamp)
+        print("Packing web_static to {}".format(filename))
+        local("tar -cvzf {} web_static".format(filename))
+        return filename
+    except Exception as ex:
+        return None
