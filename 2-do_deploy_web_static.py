ecf21f04888b3c6975822c953a83e8484c6f66a0
Ebube-Ochemba <ebube17@gmail.com>
2024-05-06 21:28:32 +0100

    pycodestyle and remove sudo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,6 +5,7 @@ A Fabric script that deploys an archive to my web servers.
 
 from fabric.api import put, run, env
 from os.path import exists, basename
+
 env.hosts = ['52.86.25.3', '35.153.194.184']
 
 
@@ -15,36 +16,32 @@ def do_deploy(archive_path):
         return False
 
     try:
-        # Extract the filename from the path
+        # Extract filename and name, store directory for archive in variable
         filename = basename(archive_path)
-
-        # Remove periond from filename
         name = filename.split(".")[0]
-
-        # Store directory for archive in variable
         path = "/data/web_static/releases/"
 
         # Upload archive to the /tmp/ directory on web servers
         put(archive_path, '/tmp/')
 
         # Create a directory for the archive on the web servers
-        run('sudo mkdir -p {}{}/'.format(path, name))
+        run('mkdir -p {}{}/'.format(path, name))
 
         # Extract the archive contents to the directory created
-        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(filename, path, name))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(filename, path, name))
 
         # Remove the archive from the /tmp/ directory
-        run('sudo rm /tmp/{}'.format(filename))
+        run('rm /tmp/{}'.format(filename))
 
         # Moves contents of 'web_static' to archive directory
-        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, name))
 
-        # Removes web_static directory.
-        run('sudo rm -rf {}{}/web_static'.format(path, name))
+        # Removes web_static directory
+        run('rm -rf {}{}/web_static'.format(path, name))
 
         # Removes existing and create new symbolic link
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, name))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, name))
 
         print("New version deployed!")
         return True
