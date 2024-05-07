22a0427a4f9c28587b27847a3a51b61f3ebd2d21
Ebube-Ochemba <ebube17@gmail.com>
2024-05-07 04:26:05 +0100

    first test

---
+++ 100-clean_web_static.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+A Fabric script that deletes out-of-date archives.
+"""
+
+from fabric.api import run, env, local
+
+env.hosts = ['52.86.25.3', '35.153.194.184']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
+
+def do_clean(number=0):
+    """Deletes out-of-date archives"""
+
+    try:
+        number = int(number)
+        if number < 0:
+            return False
+
+        # Delete archives locally
+        local("ls -t versions | tail -n +{} | "
+              "xargs -I {{}} rm versions/{{}}".format(number + 1))
+
+        # Delete archives locally
+        releases_path = "/data/web_static/releases"
+        run("ls -t {} | tail -n +{} | "
+            "xargs -I {{}} rm -rf {}/{{}}".format(releases_path, number + 1,
+                                                  releases_path))
+
+        return True
+    except Exception as e:
+        return False
